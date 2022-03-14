2c9093a0ac00947a15c33cac41a13d01ccc8b907
omyahro <omyah.ro@gmail.com>
2022-03-14 13:44:03 -0400

    Add files via upload

---
+++ login221.js
@@ -0,0 +1,35 @@
+function display(form){
+    if (form.username.value=="root") { 
+            if (form.password.value=="root") {              
+                                            location="http://localhost:5502/home.html" 
+                                                } else {
+                                                        alert("Invalid Password")
+                                                        }
+                                    } else {  alert("Invalid Username")
+                                            }
+                            }
+    
+    
+    function display(form2){
+    if (form2.username.value=="B") { 
+            if (form2.password.value=="2") {              
+                                            location="http://localhost:5502/home.html" 
+                                                } else {
+                                                        alert("Invalid Password")
+                                                        }
+                                    } else {  alert("Invalid Username")
+                                            }
+                                        }
+    
+    
+    function display(form3){
+    if (form3.username.value=="A") { 
+            if (form3.password.value=="4") {              
+                                            location="http://localhost:5502/home.html" 
+                                                } else {
+                                                        alert("Invalid Password")
+                                                        }
+                                    } else {  alert("Invalid Username")
+                                            }
+                                        }
+    
